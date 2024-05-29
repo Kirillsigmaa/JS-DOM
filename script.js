1451125782b55f3c378f165ff6eb0515f4079dea
Kirillsigmaa <ermolaevk998@gmail.com>
2024-05-29 15:45:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+//Задача 1
+// let element = document.getElementById("A")
+// element.innerHTML = "AAAAAAБОБА?"
+
+//Задача 2
+// let element = document.getElementById("div");
+// element.setAttribute("class","dives");
+
+//Задача 3
+// let element = document.getElementById("B");
+// element.setAttribute("class","far");
+
+//Задача 4
+// let element = document.getElementById("L")
+// element.innerHTML = "Унга юнга хаАХАХАХАХАХАХАХАХАХАХААХАХАХАХ"
+
+//Задача 5
+// function button_plus() {
+//     let element = document.createElement("li")
+//     let ul = document.getElementById("ul")
+//     element.innerHTML = "челик"
+//     ul.append(element)
+// }
+
+//Задача 6
+// function button_del(){
+//     let trash = document.getElementById("musor")
+//     trash.remove()
+// }
+
+//Задача 7
+// function button_mem(){
+//     let element = document.getElementById("img")
+//     element.setAttribute("src","img/mem2.jpg")
+// }
+
+//Задача 9
+// let element = document.getElementById("text");
+// function button_izmen(){
+//     if(!element.hasAttribute("class")){
+//         element.setAttribute("class","men");
+//     }else{
+//         element.removeAttribute('class')
+//     }
+// }
+
+//Задача 10
+// let element = document.getElementById("one");
+// function button_izmena(){
+//     element.innerHTML = "ВОТ ИМЕННО, НИЧЕГО НЕ ПОМЕНЯЕТСЯ!!! АААХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХАХХАХАХАХАХАХАХАХАХАХАХАХААХАХХАХАХААХАХХАХААХХАХАХАХААХАХХАХАХАХААХАХАХХАХАХАХАХАХАХАХАХАХААХ"
+// }
+
+//Задача 11
+// let element = document.getElementById("menu");
+// function button_ubrat(){
+//     if (element.hidden) {
+//         element.hidden = false
+//     }
+//     else{
+//         element.hidden = true
+//     }
+// }
+
+//Задача 12
+// function button_enter(){
+//     let elemH = document.getElementById("abzac");
+//     let elemS = document.getElementById("stroka");
+//     elemH.innerHTML = elemS.value;
+// }
+
+//Задача 13
+// let elemS = document.getElementById("chislo");
+// let m = 0
+// function button_enter2(){
+//     m++;
+//     elemS.innerHTML = m;
+// }
+
+//Задача 14
+// let elemH = document.getElementById("WWW");
+// function button_O_nas(){
+//     elemH.innerHTML = "Здесь что то написано про нас"
+// }
+
+// function button_Uslugi(){
+//     elemH.innerHTML = "Здесь что то написано про наши услуги"
+// }
+
+// function button_Kontaks(){
+//     elemH.innerHTML = "Здесь что то написано про контакты"
+// }
